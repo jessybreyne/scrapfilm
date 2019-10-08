7489937ccacf478391ba50ebd9d94b02b093e278
Axel SERRIE <axel.serrie@gmail.com>
2019-10-08 15:44:17 +0200

    Ajout du model Movie

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Movies(models.Model):
+    name = models.CharField(max_length=50)
+    img = models.URLField(max_length=200)
+    rate = models.IntegerField(null=True)
+    
+    def __str__(self):
+        return self.name
