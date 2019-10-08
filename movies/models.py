62a6e6ed46e4675765c0945264e3f30b360ae170
Axel SERRIE <axel.serrie@gmail.com>
2019-10-08 16:15:35 +0200

    ModificationMovie ajout d'un champ année

--- movies/models.py
+++ movies/models.py
@@ -7,6 +7,7 @@ class Movies(models.Model):
     name = models.CharField(max_length=50)
     img = models.URLField(max_length=200)
     rate = models.IntegerField(null=True)
+    years = models.DateField(null=True,auto_now=False, auto_now_add=False)
 
     def __str__(self):
         return self.name
