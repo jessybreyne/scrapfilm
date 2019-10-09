44a1faa905cc2d655ac8f7b10e85495e8c91613f
Axel Serrie <aserrie@info23-14.iut45.univ-orleans.fr>
2019-10-09 17:02:31 +0200

    Ajout des tests de remove dans BD

--- movies/tests.py
+++ movies/tests.py
@@ -10,17 +10,25 @@ class MovieTestCase(TestCase):
     
     # Vérification de chaque donnée et du str
     def test_affichageMovies(self):
+        #INIT MOVIES
         joker = Movies.objects.get(name="Joker")
+        fightclub = Movies.objects.get(name="FightClub")
+        #TEST WITH JOKER
         self.assertEqual(str(joker),"Joker 2019-10-09")
         self.assertEqual(joker.name,"Joker")
         self.assertEqual(joker.years,date(2019,10,9))
         self.assertEqual(joker.rate,80)
         self.assertEqual(joker.description,"C'est le joker qui est un méchant fou")
         self.assertEqual(joker.img,"#")
-        fightclub = Movies.objects.get(name="FightClub")
+        #TEST WITH FIGHTCLUB
         self.assertEqual(str(fightclub),"FightClub 1999-03-18")
         self.assertEqual(fightclub.name,"FightClub")
         self.assertEqual(fightclub.years,date(1999,3,18))
         self.assertEqual(fightclub.rate,97)
         self.assertEqual(fightclub.description,"Brandon se tappe sur la gueule")
         self.assertEqual(fightclub.img,"#")
+    
+    def test_remove_Movies(self):
+        joker = Movies.objects.get(name="Joker")
+        fightclub = Movies.objects.get(name="FightClub")
+        self.assertEqual(joker.delete(),(1,{'movies.Movie_has_Actor': 0, 'movies.Movies': 1}))
