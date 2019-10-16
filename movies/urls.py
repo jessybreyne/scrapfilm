0556162693a55446f395ab3b178de98f1c8c9172
Axel Serrie <aserrie@info21-08.iut45.univ-orleans.fr>
2019-10-16 09:14:55 +0200

    Modification diverses

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import index,addMovie,MoviesDetailView
+from .views import index,MoviesDetailView
 
 urlpatterns=[
     path('',index,name='index'),
-    path('add/',addMovie,name='addMovie'),
     path('detailsMovies/<int:pk>',MoviesDetailView.as_view(),name='details')
 ]
