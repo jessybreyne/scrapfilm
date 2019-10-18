9c52299af59bec08d0dfa208087e3554152ebad7
Axel Serrie <aserrie@info23-13.iut45.univ-orleans.fr>
2019-10-18 09:32:12 +0200

    Préparation de l'admin

--- movies/admin.py
+++ movies/admin.py
@@ -4,4 +4,21 @@ from movies.models import Movies,Actor,Movie_has_Actor,ScrappingLoader
 
 class MovieAdmin(admin.ModelAdmin):
     list_display=('name','img','rate','years','description')
+class ActorAdmin(admin.ModelAdmin):
+    list_display=('first_name','surname','img')
+class MovieHasActorAdmin(admin.ModelAdmin):
+    list_display=('movie','acteur')
+class Scrap(admin.ModelAdmin):
+    change_form_template = "admin/Scrap.html"
+
+    def response_change(self, request, obj):
+        if "scrap" in request.POST:
+            ScrappingLoader().toDb(request.POST['page'])
+            self.message_user(request, "Le scrapping s'est bien réalisé.")
+            return HttpResponseRedirect(".")
+        return super().response_change(request, obj)
+
+
 admin.site.register(Movies,MovieAdmin)
+admin.site.register(Actor,ActorAdmin)
+admin.site.register(Movie_has_Actor,MovieHasActorAdmin)
