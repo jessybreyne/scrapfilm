fc14b1ee849953ab7eaa5d603de8d94e7f84d8ef
Axel Serrie <aserrie@info23-13.iut45.univ-orleans.fr>
2019-10-18 08:51:24 +0200

    Ajout de l'admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from movies.models import Movies,Actor,Movie_has_Actor,ScrappingLoader
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display=('name','img','rate','years','description')
+admin.site.register(Movies,MovieAdmin)
